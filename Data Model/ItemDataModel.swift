4972bb88d3de7f9f01f5ff4765cf60010bbc3425
Dragan Milovanovic <draganmilovan381@gmail.com>
2018-09-24 19:46:02 +0200

    Created Items Data Model

---
+++ Data Model/ItemDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  Data Model.swift
+//  AR Furniture
+//
+//  Created by Dragan Milovanovic on 9/24/18.
+//  Copyright © 2018 Dragan Milovanovic. All rights reserved.
+//
+
+import Foundation
+
+
+struct ItemDataModel {
+    
+    let name: String
+    let seriesTag: String
+    let categoryTag: String
+    let descriptionTags: [String]
+    
+}
