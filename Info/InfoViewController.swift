9b61f2f65a6c8bcd637a47062f4cec7e6001f861
Igor Aleksic <igaleksic@gmail.com>
2018-09-25 18:36:26 +0200

    Created updateUI

--- Info/InfoViewController.swift
+++ Info/InfoViewController.swift
@@ -22,7 +22,19 @@ class InfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateUI () {
+        name.text = infoDataManager.items[0].name
+        address.text = infoDataManager.items[0].address
+        tel.text = infoDataManager.items[0].tel
+        email.text = infoDataManager.items[0].email
+        hours.text = infoDataManager.items[0].hours
+        
+        
     }
     
     @IBAction func myLocation(_ sender: Any) {
