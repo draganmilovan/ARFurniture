a57134e65b2a83643ea95d3f4124a325a9062c23
Dragan Milovanovic <draganmilovan381@gmail.com>
2018-09-26 22:46:51 +0200

    Added new properties and methods in Items Data Manager

--- Data Model/ItemsDataManager.swift
+++ Data Model/ItemsDataManager.swift
@@ -11,22 +11,59 @@ import Foundation
 
 final class ItemsDataManager {
     
-    var items = [ItemDataModel]()
-    
-    init () {
-        populateItems()
+    var items: [ItemDataModel]? {
+        didSet {
+            populateCategories()
+            populateSeries()
         }
+    }
     
-    private func populateItems() {
-        let url = Bundle.main.path(forResource: "ItemsData", ofType: "json")!
-        do{
+    var categories: [String] = []
+    var series: [String] = []
+    
+    
+    init() {
+        populateItems()
+    }
+    
+}
+
+
+// MARK:- Items Data Manager private methods
+fileprivate extension ItemsDataManager {
+    
+    //
+    // Method for populating items Array
+    //
+    func populateItems() {
+        guard let url = Bundle.main.path(forResource: "ItemsData", ofType: "json") else {
+            fatalError("Missing ItemsData JSON!")
+        }
+        do {
             let data = try Data(contentsOf: URL(fileURLWithPath: url))
             
             self.items = try JSONDecoder().decode([ItemDataModel].self, from: data)
         }
-        catch{
+        catch {
             print("JSON error")
         }
+    }
+    
+    
+    //
+    // Method for populating categories Array
+    //
+    func populateCategories() {
+        //guard let items = items else { return }
+
+    }
+    
+    
+    //
+    // Method for populating series Array
+    //
+    func populateSeries() {
+        
     }
     
 }
