31c0a0db6348b1d5fee24aaea92c3a713fa0599c
Dragan Milovanovic <draganmilovan381@gmail.com>
2018-10-04 14:56:00 +0200

    Created Item Controller

---
+++ AR Furniture/ItemController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemController.swift
+//  AR Furniture
+//
+//  Created by Dragan Milovanovic on 10/4/18.
+//  Copyright © 2018 Dragan Milovanovic. All rights reserved.
+//
+
+import UIKit
+
+class ItemController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
