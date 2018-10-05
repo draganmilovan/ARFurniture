3447c40cd457c9118fc1bcd315de256a0fe2c152
Dragan Milovanovic <draganmilovan381@gmail.com>
2018-10-05 16:35:07 +0200

    Created Catalog Table View Controller

---
+++ AR Furniture/CatalogTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CatalogTableViewController.swift
+//  AR Furniture
+//
+//  Created by Dragan Milovanovic on 10/5/18.
+//  Copyright © 2018 Dragan Milovanovic. All rights reserved.
+//
+
+import UIKit
+
+class CatalogTableViewController: UIViewController {
+    
+    @IBOutlet weak var catalogTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
