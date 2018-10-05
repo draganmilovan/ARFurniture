1d4e53df4cbc82ab0a8cc9ea1b440b831df6599b
Igor Aleksic <igaleksic@gmail.com>
2018-10-05 17:33:08 +0200

    Changed StoreInfo JSON

--- Info/InfoViewController.swift
+++ Info/InfoViewController.swift
@@ -20,7 +20,7 @@ class InfoViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var tableView: UITableView!
@@ -28,36 +28,36 @@ class InfoViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
-addAnnotations()
+        
+        addAnnotations()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count-1]
-       
+        
         if location.horizontalAccuracy > 0 {
-           
+            
             locationManager.stopUpdatingLocation()
             closestDestination()
             
         }
     }
-   
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
     {
         print("Error \(error)")
         locationStatusLbl.text = "Nije moguće utvrditi lokaciju"
     }
-
     
-        
-
+    
+    
+    
     @IBAction func myLocation(_ sender: Any) {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
-       if CLLocationManager.locationServicesEnabled() {
-        locationManager.startUpdatingLocation()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
         }
         
         
@@ -66,9 +66,9 @@ addAnnotations()
         
         let storeCoordinates = infoDataManager.items.map{$0.coordinates}
         
-        let new = locationManager.location 
-    
-    
+        let new = locationManager.location
+        
+        
         
         let loc = storeCoordinates.map{CLLocation(latitude: $0.lat, longitude: $0.long)}
         
@@ -85,7 +85,7 @@ addAnnotations()
             mapView.setRegion(coordinateRegion, animated: true)
         }
         centerMapOnLocation(location: storeLocation!)
-
+        
     }
     
     //tableview
@@ -111,31 +111,32 @@ addAnnotations()
             mapView.setRegion(coordinateRegion, animated: true)
         }
         centerMapOnLocation(location: storeLocation)
-
+        
     }
     
-        
-        
-        
-        func addAnnotations(){
-            let storeCoordinates = infoDataManager.items.map{$0.coordinates}
-            let coords = storeCoordinates.map{CLLocation(latitude: $0.lat, longitude: $0.long)}
-            for coord in coords{
-                let CLLCoordType = CLLocationCoordinate2D(latitude: coord.coordinate.latitude,
-                                                          longitude: coord.coordinate.longitude);
-                let anno = MKPointAnnotation()
-                anno.coordinate = CLLCoordType
-                mapView.addAnnotation(anno)
-            }
-            let regionRadius: CLLocationDistance = 500000
-            let locMap = CLLocationCoordinate2D(latitude: 44.809909, longitude: 20.433341)
-                let coordinateRegion = MKCoordinateRegion(center: locMap , latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-                mapView.setRegion(coordinateRegion, animated: false)
-            
-        }
-        
-        
-        
     
-   
+    
+    
+    func addAnnotations(){
+        let storeCoordinates = infoDataManager.items.map{$0.coordinates}
+        let coords = storeCoordinates.map{CLLocation(latitude: $0.lat, longitude: $0.long)}
+        for coord in coords{
+            let CLLCoordType = CLLocationCoordinate2D(latitude: coord.coordinate.latitude,
+                                                      longitude: coord.coordinate.longitude);
+            let anno = MKPointAnnotation()
+            anno.coordinate = CLLCoordType
+            mapView.addAnnotation(anno)
+        }
+        let regionRadius: CLLocationDistance = 500000
+        let locMap = CLLocationCoordinate2D(latitude: 44.809909, longitude: 20.433341)
+        let coordinateRegion = MKCoordinateRegion(center: locMap , latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: false)
+        
+    }
+    
+    
+    
+    
+    
 }
+
